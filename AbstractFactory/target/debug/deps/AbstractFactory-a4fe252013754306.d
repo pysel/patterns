fdea34e0aea4685efaf9c5eb5b1d558838a61dac
Ruslan <rakhtariev@icloud.com>
2023-03-18 20:44:12 +0700

    saving progress + adapter

---
+++ AbstractFactory/target/debug/deps/AbstractFactory-a4fe252013754306.d
@@ -0,0 +1,6 @@
+/Users/ruslanakhtariev/github/patterns/AbstractFactory/target/debug/deps/AbstractFactory-a4fe252013754306.rmeta: src/main.rs src/car.rs
+
+/Users/ruslanakhtariev/github/patterns/AbstractFactory/target/debug/deps/AbstractFactory-a4fe252013754306.d: src/main.rs src/car.rs
+
+src/main.rs:
+src/car.rs:
---
+++ AbstractFactory/target/debug/deps/AbstractFactory-e9f8f108bce2f16c.d
@@ -0,0 +1,6 @@
+/Users/ruslanakhtariev/github/patterns/AbstractFactory/target/debug/deps/AbstractFactory-e9f8f108bce2f16c.rmeta: src/main.rs src/car.rs
+
+/Users/ruslanakhtariev/github/patterns/AbstractFactory/target/debug/deps/AbstractFactory-e9f8f108bce2f16c.d: src/main.rs src/car.rs
+
+src/main.rs:
+src/car.rs:
